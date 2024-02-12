dbf7c6d26cf2c7b6f93184e4487133830a021a67
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-02-12 13:10:37 -0700

    changed name

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import './NavBar.css'
 const NavBar: React.FC = () => {
   return (
     <nav className="navbar">
-      <h1 className="navbar-title">Passwords & Passwords & Passwords</h1>
+      <h1 className="navbar-title">Passwords Passwords Passwords</h1>
       {/* Navigation links removed, leaving only the title */}
     </nav>
   )
